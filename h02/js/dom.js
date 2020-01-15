fcb27d6b2e338c40688a3c802b8b2ad56e1a1ed8
honman21 <hdeong@roc-dev.com>
2020-01-15 09:03:22 +0100

    js

---
+++ h02/js/dom.js
@@ -0,0 +1,11 @@
+var paginatitel = {
+    tekst: "Dit is de paginatitel"
+}
+var subkop = {
+    tekst: "Dit is de subkop"
+}
+
+document.getElementById("grotekop").innerHTML = paginatitel.tekst;
+document.getElementById("subkop").innerHTML = subkop.tekst;
+document.getElementById("pudding1").src = "img/pudding1.jpg";
+document.getElementById("pudding2").src = "img/pudding2.jpg";
