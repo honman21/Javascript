47be90ae1e4962ef55aae8cdcf068c1c797cf399
honman21 <hdeong@roc-dev.com>
2020-01-15 13:13:04 +0100

    js

---
+++ h02/js/dom2.js
@@ -0,0 +1,21 @@
+
+var blok1 = document.getElementById('b1');
+blok1.style.backgroundColor = "cyan";
+blok1.style.textAlign = "center";
+blok1.style.paddingTop= "20px";
+
+var blok2 = document.getElementById('b2');
+blok2.style.backgroundColor = "green";
+blok2.style.marginTop = "20px"
+blok2.style.textAlign = "center";
+
+var blok3 = document.getElementById('b3');
+blok3.style.backgroundColor = "yellow";
+blok3.style.marginTop = "40px"
+blok3.style.textAlign = "right";
+blok3.style.paddingTop= "0px";
+
+var blok3 = document.getElementById('b4');
+blok3.style.backgroundColor = "red";
+blok3.style.textAlign = "left";
+blok3.style.paddingTop= "0px";
